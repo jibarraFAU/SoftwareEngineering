1303a39a5f7b051ccb337eeff228ec63c4f84091
jibarraFAU <jibarra2014@fau.edu>
2020-11-06 18:03:02 -0500

    Introduced Bower Components and fixed angular bug

---
+++ Project/client/app/components/emotes/emotes.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'emotes',
+    templateUrl: 'emotes.component.html'
+})
+export class EmotesComponent { }
--- Project/server.js
+++ Project/server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var emotes = require('./routes/emotes');
 
-var port = 8080;
+var port = 3000;
 
 var app = express();
 
